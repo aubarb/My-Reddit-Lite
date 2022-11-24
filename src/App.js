78a2e9b65600f2b7e8a34049936dbe554252133c
jisokoo <abarbancon29@gmail.com>
2022-11-24 15:24:48 +0100

    refactor code, improve code readibility and implement best practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 import React, { useEffect, useState,  } from 'react';
 import Post from './Components/Post';
-import Categories from './Components/Categories';
-import Search from './Components/Search';
+
+const categoryItems = [
+  ["r/investing", "Investing"],
+  ["r/Economics", " Economics"],
+  ["r/StockMarket", "Stock Market"],
+  ["r/dividends", "Divivends"],
+  ["r/wallstreetbets", "Wall St Bets"],
+  ["r/CryptoCurrency", "Crypto"],
+  ["r/webdev", "Web Dev"],
+  ["r/frontend", "Front-End"],
+  ["r/javascript", "Javascript"],
+  ["r/AskProgramming", "Ask Progr"],
+  ["r/css", "CSS"],
+  ["r/Cooking", "Cooking"]
+];
+
+const filterItems = [
+  ["/hot", "Hot"],
+  ["/new", "New"],
+  ["/top", "top"]
+];
 
 function App() {
-
   const [publications, setPublications] = useState([]);
   const [category, setCategory] = useState("r/investing");
-  const [search, setSearch] = useState("")
-  const [filter, setFilter] = useState('');
-  const [theme, setTheme] = useState('light');
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("");
+  const [theme, setTheme] = useState("light");
 
-  console.log(filter);
-  //function to format fetched data correctly and be reused in fetch function.
-  const formatData = (data) => {
-    let p = [];
-    data.forEach(publication => {
-      p.push({
+  const formatData = (data) =>
+    data.map(publication => ({
         id: publication.data.id,
         author: publication.data.author,
         title: publication.data.title,
@@ -31,112 +45,119 @@ function App() {
         text: publication.data.selftext,
         isSelf: publication.data.is_self,
         url: publication.data.url,
-      })
-    })
-    return p
-  }
+    }));
 
-  // Fetch data from reddit.json link and according to category state. Rceive an array of 15 objects each containing all info about 1 post. Looping over this data to push to p only the data needed and setting the publication state.
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(`https://www.reddit.com/${category}${filter}.json?limit=30`);
       const json = await res.json();
-      const data = json.data.children;
-      console.log(data);
-      let p = formatData(data)
-      setPublications(p);
+      setPublications(formatData(json.data.children));
     }
     fetchData();
-  }, [category, filter])
+  }, [category, filter]);
 
-  //Change category state when user selects a category
-  const handleCategory = (value) => {
-    setCategory(value)
-  };
-
-  //Change filter state when user selects a filter
-  const handleFilter = (value) => {
-    setFilter(value);
-  };
-
-  //Set search state when user searches
   const handleChange = (event) => setSearch(event.target.value);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     async function fetchData() {
       const res = await fetch(`https://www.reddit.com/search.json?q=${search}&limit=30`);
       const json = await res.json();
-      const data = json.data.children;
-      let p = formatData(data)
-      setPublications(p);
+      setPublications(formatData(json.data.children));
     }
     fetchData();
-  }
-
-  const switchTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-  }
-
-  //Use data from publications state to create <Post /> components and pass down needed props.
-  const publicationsEl = publications.map(publication => {
-    if (publication.isVideo === false) {
-      return (
-        <Post 
-          key={publication.id}
-          author={publication.author}
-          title={publication.title}
-          score={publication.score}
-          creationDate={publication.creationDate}
-          numComments={publication.numComments}
-          permalink={publication.permalink}
-          isVideo={publication.isVideo}
-          media={publication.media}
-          isMedia={publication.isMedia}
-          text={publication.text}
-          isSelf={publication.isSelf}
-          url={publication.url}
-        />
-      )
-    }
-  })
+  };
 
   return (
     <div className="app" data-theme={theme}>
       <header id="top">
-        <img onClick={(event) => handleCategory("r/investing")} className="logo" src={require("./resources/images/reddit-logo.png")} />
-        <Search handleSubmit={handleSubmit} handleChange={handleChange} />
+        <img 
+          onClick={() => setCategory("r/investing")} 
+          className="logo"
+          alt="Reddit Logo"
+          src={require("./resources/images/reddit-logo.png")} 
+        />
+        <form className="search-section">
+            <input placeholder="Search for..." onChange={handleChange} />
+            <button onClick={handleSubmit} >
+              <img 
+                src={require("./resources/images/search-icon.png")}
+                alt="Search icon"
+              />
+            </button>
+        </form>
         <div className='filters'>
-          <h2 onClick={(event) => handleFilter("/hot")}>Hot</h2>
-          <h2 onClick={(event) => handleFilter("/new")}>New</h2>
-          <h2 onClick={(event) => handleFilter("/top")}>Top</h2>
+          {filterItems.map(([filter, filterName]) => {
+            return <h2 onClick={() => setFilter(filter)}>{filterName}</h2>
+          })}
         </div>
-        <img className='dark-mode' src={theme === "light" ? require("./resources/images/moon.png") : require("./resources/images/sun.png")} onClick={switchTheme} />
+        -{" "}
+        <img
+          alt="dark mode icon"
+          className="dark-mode "
+          src={
+            theme === "light" 
+              ? require("./resources/images/moon.png") 
+              : require("./resources/images/sun.png")
+          } 
+          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
       </header>
       <div className='route'>
-        <h3>{category} > {filter}</h3>
+        <h3>
+          {category} &gt; {filter}
+        </h3>
       </div>
       <div className='category-section'>
         <h1 className='r'>/r</h1>
-        <Categories handleClick={handleCategory}/>
+        <div className="category-block">
+            {categoryItems.map(([category, categoryName]) => {
+              return (
+                <div  
+                  className="category-item" 
+                  onClick={() => setCategory(category)} 
+                >
+                  <h3>{categoryName}</h3>
+                </div>
+              );
+            })}
+        </div>
       </div>
       <div className='upArrow'>
-        <a href="#top"><img  src={theme === "light" ? require("./resources/images/upArrow-dark.png") : require("./resources/images/upArrow-light.png")} /></a>
+        <a href="#top">
+          <img 
+            alt="Top arrow"
+            src={
+              theme === "light" 
+                ? require("./resources/images/upArrow-dark.png") 
+                : require("./resources/images/upArrow-light.png")
+            } 
+          />
+        </a>
       </div>
       <div className='post-section'>
-        {publicationsEl}
+        {publications.map((publication) => {
+          if (!publication.isVideo) {
+            return (
+              <Post 
+                key={publication.id}
+                author={publication.author}
+                title={publication.title}
+                score={publication.score}
+                creationDate={publication.creationDate}
+                numComments={publication.numComments}
+                permalink={publication.permalink}
+                isMedia={publication.isMedia}
+                text={publication.text}
+                isSelf={publication.isSelf}
+                url={publication.url}
+              />
+            );
+          }
+        })}
       </div>
     </div>
   );
 }
 
 export default App;
-
-/*
-.media.reddit_video
-.dash_url: "https://v.redd.it/ehmfrqaeg5z91/DASHPlaylist.mpd?a=1670749640%2CZjc2YTA1OGNjODYwZWVkNzFlYjNjODQ0ZTc2NTU5YWY4MTFlMjg5MDM1ZjY1Y2Q4OGZhYzVlMmMwMjUzZDBiYw%3D%3D&amp;v=1&amp;f=sd"
-.fallback_url: "https://v.redd.it/ehmfrqaeg5z91/DASH_1080.mp4?source=fallback"
-.hls_url: "https://v.redd.it/ehmfrqaeg5z91/HLSPlaylist.m3u8?a=1670749640%2CN2I5MWYyZWY2MzBmNDFmMTcyM2RjNjI0NTVkNWE5YzcxYTc5MWVkOGUwMzc2OTA5ZjQzZDc4YzA2NGI0M2RlNQ%3D%3D&amp;v=1&amp;f=sd"
-.scrubber_media_url: "https://v.redd.it/ehmfrqaeg5z91/DASH_96.mp4"
-.secure_media.reddit_video
-*/
--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,78 +1,87 @@
 import React, { useState, useEffect } from "react";
-import Comment from "./Comment";
 
-export default function Post(props) {
-
-    const { author, title, creationDate, score, numComments, isVideo, isMedia, isSelf, media, url, text, permalink } = props
+export default function Post({
+    author, 
+    title, 
+    creationDate, 
+    score, 
+    numComments, 
+    isMedia, 
+    isSelf, 
+    url, 
+    text, 
+    permalink
+}) {
 
     const [comments, setComments] = useState([]);
     const [displayComments, setDisplayComments] = useState(false);
 
-    // Format date: input is time posted in second since epoch, output is hours or days since posted.
     const dateFormating = () => {
         let hoursSincePosted = ((Date.now() / 1000) - creationDate) / 3600 ;
-        if (hoursSincePosted < 24) {
-            return Math.round(hoursSincePosted) + "h ago";
-        } else {
-            return Math.round(hoursSincePosted / 24) + "d ago"
-        }
-    }
+        return hoursSincePosted < 24 
+            ? Math.round(hoursSincePosted) + "h ago"
+            : Math.round(hoursSincePosted / 24) + "d ago"
+    };
 
-    //Fetch all comments for the post and set comments state
     useEffect(() => {
         async function fetchComments() {
             const res = await fetch(`https://www.reddit.com${permalink}.json`);
             const json = await res.json();
             const data = json[1].data.children
-            let c = [];
-            data.forEach(comment => {
-                c.push({
+            setComments(
+                data.map((comment) => ({
                     body: comment.data.body,
                     author: comment.data.author,
-                })
-            })
-            setComments(c)
+                }))
+            )
         }
         fetchComments();
     }, [permalink])
-
-    //Display or hide comments on click
-    const handleDisplayComments = () => {
-        setDisplayComments(!displayComments)
-    }
-
-    //Map over comments state to render a comment component
-    const commentsEl = comments.map(comment => {
-        return (
-            <Comment body={comment.body} author={comment.author} />
-        )
-    })
-
+    
     return(
         <div className="post-block">
             <h1 className="post-title">{title}</h1>
             <div className="post-content">
                 {
-                    isMedia 
-                        ? <img className="post-image" src={url} />
-                        : text 
-                            ? <p>{text}</p>
-                            : isSelf
-                                ? ""
-                                : <a href={url}>{url}</a>
+                    (() => {
+                        if (isMedia ) {
+                            return <img alt="" className="post-image" src={url} />
+                        }
+                        if (text) {
+                            return <p>{text}</p>
+                        }
+                        if (isSelf) {
+                            return ""
+                        }
+                        return <a href={url}>{url}</a>
+                    })
                 }
             </div>
             <div className="post-info">
                 <h4 className="post-votes">Score: {score}</h4>
                 <h4 className="post-author">By {author}</h4>
                 <h4 className="post-date">{dateFormating()}</h4>
-                <h4 className="post-comments" onClick={handleDisplayComments} >{numComments} comments</h4>
+                <h4 
+                    className="post-comments" 
+                    onClick={() => setDisplayComments(!displayComments)} 
+                >
+                    {numComments} comments
+                </h4>
             </div>
             {
                 displayComments &&
                 <div className="comment-block" >
                     <h2>Comments:</h2>
-                    {commentsEl}
+                    {
+                        comments.map(({ body, author }) => {
+                            return (
+                                <div className="comment-item">
+                                    <h4>{author}:</h4>
+                                    <p>{body}</p>
+                                </div>
+                            );
+                        })
+                    }
                 </div>
             }
         </div>
